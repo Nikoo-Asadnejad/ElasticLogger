9b02e719f87b8ec85888b84ad6b43950856dd5df
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2025-07-10 12:07:59 +0330

    feat: add rabbit consumer

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"logger-service/elasticlogger"
 	"net/http"
+
+	"github.com/streadway/amqp"
 )
 
 var logService elasticlogger.ILogger
@@ -17,6 +19,8 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to create logger:", err)
 	}
+
+	go startRabbitMQConsumer()
 
 	http.HandleFunc("/log", handleLog)
 
@@ -46,4 +50,65 @@ func handleLog(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintln(w, "Log entry created")
+}
+
+func startRabbitMQConsumer() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"log_entries", // queue name
+		true,          // durable
+		false,         // auto-delete
+		false,         // exclusive
+		false,         // no-wait
+		nil,           // arguments
+	)
+
+	if err != nil {
+		log.Fatalf("Failed to declare a queue: %v", err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue name
+		"",     // consumer tag
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // arguments
+	)
+	if err != nil {
+		log.Fatalf("Failed to register a consumer: %v", err)
+	}
+
+	fmt.Println("RabbitMQ consumer started. Waiting for messages...")
+	for msg := range msgs {
+		var entry elasticlogger.LogEntry
+		err := json.Unmarshal(msg.Body, &entry)
+		if err != nil {
+			log.Printf("Failed to parse message: %v", err)
+			continue
+		}
+
+		err = logService.Log(entry)
+		if err != nil {
+			log.Printf("Failed to log entry: %v", err)
+		} else {
+			fmt.Println("Log entry successfully logged:", entry)
+		}
+	}
 }
